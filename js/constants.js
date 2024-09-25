d68e1e3ca073fc719a478e8ed1fe4137cc7775fa
Ananasaaa <kairyakn@gmail.com>
2024-09-25 14:30:20 +0300

    task2

---
+++ js/constants.js
@@ -0,0 +1,12 @@
+export const commentsText = [
+    'This picture is amazing!',
+    'What a beautiful sunset!',
+    'I love the colors in this photo.',
+    'This place looks so peaceful.',
+    'You captured the moment perfectly.',
+    'This photo tells a story.',
+    'I wish I were there.',
+    'Stunning view!'
+];
+
+export const names = ['John', 'Alex', 'Mary', 'Jake', 'Bill', 'Kate', 'Mike', 'Lola', 'George', 'Andy', 'Jessica'];
